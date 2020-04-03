e3f7661e62120e064cfab00cbe8d380eb8d22674
rod <rodnascimentolopes@gmail.com>
2020-04-02 23:30:54 -0300

    Front-end do app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './global.css';
+
+import Routes from './routes';
+
+function App() { //componente no react - uma função que retorna HTML
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes />
   );
 }
 
 export default App;
+
+/*
+//componente no react - uma função que retorna HTML
+//JSX (Javascript XML) - HTML integrado dentro do JS
+//props - variavel do JS - {}
+//props.children
+
+import React, { useState } from 'react';
+
+import Logon from './pages/Logon';
+
+function App() { //componente no react - uma função que retorna HTML
+  const [count, setCount] = useState(0); //transforma a variavel num State
+
+  //retorna um Array [valor, funcaoAtualizacao] //imutabilidade do State
+
+  function increment() {
+    setCount(count + 1);
+  }
+
+  return ( //state reflete as alterações imediatamente no react
+    <div> 
+      <Header>Contador: {count}</Header> 
+      <button onClick={increment}>Incrementar</button>
+    </div>
+  );
+}
+*/
